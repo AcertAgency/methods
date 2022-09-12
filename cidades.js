b1f53cbc44ddc61c01dee0e8121c0d82120e1d0b
AcertAgency <110690919+AcertAgency@users.noreply.github.com>
2022-09-12 13:33:30 -0300

    cidades.js

---
+++ cidades.js
@@ -0,0 +1,23 @@
+
+//Exemplo do arquivo
+{
+    "erro": false,
+    "cidade": {
+        "197": {
+            "nome": "ABADIA DE GOIAS",
+            "estado": "GO"
+        },
+        "25669": {
+            "nome": "ZACARIAS",
+            "estado": "SP"
+        },
+        "3407": {
+            "nome": "ZE DOCA",
+            "estado": "MA"
+        },
+        "6892123": {
+            "nome": "ZORTEA",
+            "estado": "SC"
+        }
+    }
+}
